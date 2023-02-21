b174f9037770e6b40f16f5e5294963808756ed65
Ndutawanjohi <wambuitess3@gmail.com>
2023-02-21 14:55:45 -0800

    Print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - prints 10 times the alphabet, in lowercase, 
-* then adds a new line.
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+* then adds a new line
 */
 void print_alphabet_x10(void)
 {
