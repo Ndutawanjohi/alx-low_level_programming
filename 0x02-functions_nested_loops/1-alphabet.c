dc31f861de77146d125713698c17d0387c5aa31e
Ndutawanjohi <wambuitess3@gmail.com>
2023-02-21 13:32:18 -0800

    Alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * a function that prints the alphabet, in lowercase, followed by a new line.
+ * Tassk 1: print alphabets in lowercase
+ * print_alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
  */
- 
-void print_alphabet(void)
-{
-    char c = 'a';
 
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
-    }
+void print_alphabet(void) {
+  char c = 'a';
 
-    _putchar('\n');
+  while (c <= 'z') {
+    _putchar(c);
+    c++;
+  }
+
+  _putchar('\n');
 }
