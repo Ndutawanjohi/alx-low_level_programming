626b5200cdb6376f2d7f2e1aeccc8ac2f8a2cfab
Ndutawanjohi <wambuitess3@gmail.com>
2023-02-21 13:57:34 -0800

    Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * Tassk 1: print alphabets in lowercase
  * print_alphabet -  prints the alphabet, in lowercase, followed by a new line.
  */
 
 void print_alphabet(void)
 {
   char c = 'a';
-
   while (c <= 'z')
   {
     _putchar(c);
     c++;
   }
-
   _putchar('\n');
 }
