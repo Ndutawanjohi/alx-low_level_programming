4235517294a5f632094822684ef1136068434253
Ndutawanjohi <wambuitess3@gmail.com>
2023-02-22 12:29:57 -0800

    There is no absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of integer
+ * @k :input number as integer
+ * Return: absolute value
+ */
+
+int _abs(int k)
+{
+if (k >= 0)
+{
+return (k);
+}
+else
+{
+return (k * -1);
+}
+}
